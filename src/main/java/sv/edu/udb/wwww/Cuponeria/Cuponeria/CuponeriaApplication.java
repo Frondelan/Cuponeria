bc09a70544bcf056fc047e4c48c3b00fa0f99924
Sergio <ser0012011@hotmail.com>
2018-11-23 09:52:04 -0600

    Proyecto

---
+++ src/main/java/sv/edu/udb/wwww/Cuponeria/Cuponeria/CuponeriaApplication.java
@@ -0,0 +1,12 @@
+package sv.edu.udb.wwww.Cuponeria.Cuponeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuponeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuponeriaApplication.class, args);
+	}
+}
---
+++ src/test/java/sv/edu/udb/wwww/Cuponeria/Cuponeria/CuponeriaApplicationTests.java
@@ -0,0 +1,16 @@
+package sv.edu.udb.wwww.Cuponeria.Cuponeria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CuponeriaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
