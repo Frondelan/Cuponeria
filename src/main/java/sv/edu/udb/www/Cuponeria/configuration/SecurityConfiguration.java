b27105f55272eb440a1be220c3e0aad1a44cdbcc
Sergio <ser0012011@hotmail.com>
2018-11-23 23:52:15 -0600

    Login

--- src/main/java/sv/edu/udb/www/Cuponeria/configuration/SecurityConfiguration.java
+++ src/main/java/sv/edu/udb/www/Cuponeria/configuration/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/css/**", "/js/**","/public/**","/images/**","/api/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
-			.formLogin().loginPage("/login").defaultSuccessUrl("/login")
+			.formLogin().loginPage("/login").defaultSuccessUrl("/administrador/inicio")
 			.usernameParameter("correo").passwordParameter("password")
 			.permitAll();
 			
